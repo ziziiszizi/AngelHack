c37815a738f9b56f4eb7537ee453ffa258e19845
No One <risingsoul1991@gmail.com>
2016-06-05 05:23:56 +0800

    cincai

--- index.php
+++ index.php
@@ -51,10 +51,57 @@
 	<div id="howto">
 	</div>
 	
-	<div id="contact">
+	<div id="contact" class="text-center">
+	  <h2>THE DEVELOPERS</h2>
+	  <p><em>We love to hear from you!</em></p>
+	  <p>If you have any feedback to us, feel free to contact us</p>
+	  <p>We welcome your feedback so that we can provide a better platform for you</p>
+	  <br>
+	  <div class="row">
+		<div class="col-sm-4">
+		  <h3 class="text-center"><strong>Dr. Lam Chen Riang</strong></h3><br>
+		  <a href="#demo" data-toggle="collapse">
+			<img src="image/dev1.jpg" class="img-circle person" alt="lam" width="255" height="255">
+		  </a>
+		  <div id="demo" class="collapse">
+			<h4>Main Developer</h4>
+			<h4>Best programmer in the world</h4>
+			<h4>lamchenriang@gmail.com</h4>
+		  </div>
+		</div>
+		<div class="col-sm-4">
+		  <h3 class="text-center"><strong>Ryan Lim Weng Thai</strong></h3><br>
+		  <a href="#demo2" data-toggle="collapse">
+			<img src="image/dev2.jpg" class="img-circle person" alt="ryan" width="255" height="255">
+		  </a>
+		  <div id="demo2" class="collapse">
+			<h4>Co-Developer</h4>
+			<h4>Second programmer in the world</h4>
+			<h4>eloelo@gmail.com</h4>
+		  </div>
+		</div>
+		<div class="col-sm-4">
+		  <h3 class="text-center"><strong>Tristan Tan Yao Hui</strong></h3><br>
+		  <a href="#demo3" data-toggle="collapse">
+			<img src="image/dev3.jpg" class="img-circle person" alt="hui" width="255" height="255">
+		  </a>
+		  <div id="demo3" class="collapse">
+			<h4>Co-developer</h4>
+			<h4>Third best programmer in the world</h4>
+			<h4>ttyh@yahoo.com</h4>
+		  </div>
+		</div>
+	  </div>
 	</div>
 	
-	<div id="account">
+	<div id="joinus">
+	<img src="image/joinus.jpg" alt="joinus" width="100%" height="auto"><br>
+	<div class="col-md-6">
+	<h1>Be part of our Beta Tester! </h1>
+	</div>
+	<div class="col-md-6">
+	<button style="background-color:green; font-size:36px;">Join Us Now!</button>
+	</div>
 	</div>
 </body>
 </html>
